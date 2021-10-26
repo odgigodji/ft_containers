4de5eace9f80feab9615afa317b9655776b20366
odgigodji <santa51@mail.ru>
2021-10-26 22:01:11 +0300

    constructors

--- vector.hpp
+++ vector.hpp
@@ -57,11 +57,10 @@ template<class Tp,  class Alloc = std::allocator<Tp> >
 /*
 	Parameters:
 	alloc	-	allocator to use for all memory allocations of this container
-	count	-	the size of the container
 	value	-	the value to initialize elements of the container with
 	first, last	-	the range to copy the elements from
 	other	-	another container to be used as source to initialize
- 	the elements of the container with
+ 	the elements of the container with.
 	init	-	initializer list to initialize the elements of the container with
  */
 
@@ -69,7 +68,7 @@ template<class Tp,  class Alloc = std::allocator<Tp> >
 public:
 	vector() : _arr(nullptr), _size(0), _capacity(0) {}
 
-//	2)Constructs a container with n elements. Each element is a copy of val.
+//	2)Fill constructor.Constructs a container with n elements. Each element is a copy of val.
 	vector(size_type n, const_reference value) :	_size(n), _capacity(n)
 	{
 		_arr = _alloc.allocate(n);
@@ -79,10 +78,27 @@ public:
 		}
 	}
 
-/*	3)Constructs a container with as many elements as the range (first,last),
- * with each element constructed from its corresponding element
- * in that range, in the same order. */
+//3)Range constructor.Constructs a container with as many elements as the range
+// (first,last),with each element constructed from its corresponding
+// element in that range, in the same order.
+	template<class Iter>
+	vector(Iter first, Iter last) : _size(0) {
+		size_t range = last - first;
+		if (range < 0) { throw std::out_of_range("vector"); }
+		_arr = _alloc.allocate(range);
+		_capacity = range;
+		//insert(begin(), first, last)
+	}
 
+//4)Copy constructor.Constructs a container with a copy of each of the
+// elements in x, in the same order.
+	vector(const vector& other) {
+		_arr = _alloc.allocate(_capacity);
+		for(size_t i = 0; i < _size; i++) {
+			_alloc.construct(_arr + i, other._arr[i]);
+		}
+
+	}
 
 	};
 //-----------------------------------ft---------------------------------------//
