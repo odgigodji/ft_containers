acfdc14995e66ee220b2bf4fea8c4394ed606e8f
odgigodji <santa51@mail.ru>
2021-11-06 00:43:18 +0300

    map start

--- map.hpp
+++ map.hpp
@@ -6,10 +6,28 @@
 #define FT_CONTAINERS_MAP_HPP
 #include "ft.hpp"
 
+namespace ft
+{
 template < class Key, class T, class Compare, class Allocator >
-namespace ft {
 	class map {
+		/*******************************************************************************
+		*=================================MEMBER_TYPES=================================*
+		********************************************************************************
+		*___________________________________Typedefs__________________________________*
+		*******************************************************************************/
+	public:
+		typedef Key						key_type;
+		typedef T							mapped_type;
+		typedef Compare					key_compare;
+		typedef Allocator					allocator_type;
+//		typedef ft::pair<const Key, T>	value_type;
+//		typedef value_type* 				pointer;
+//		typedef value_type&					reference;
+//		typedef const value_type*			const_pointer;
+//		typedef const value_type&			const_reference;
+//		typedef ptrdiff_t					difference_type;
 
+	};
 }
 
 #endif //FT_CONTAINERS_MAP_HPP
