5fae6c8fd793acc9194c0fdd0b30a62efc769263
odgigodji <santa51@mail.ru>
2021-11-06 00:10:19 +0300

    leak in stack constructor

--- ft_containers-unit-test/sources/utilities_tests/iterator_traits.cpp
+++ ft_containers-unit-test/sources/utilities_tests/iterator_traits.cpp
@@ -10,7 +10,6 @@ bool iterator_traits_test() {
 	res.push_back(typeid(std::vector<int>::iterator::iterator_category).name());
 	res.push_back(typeid(std::vector<int>::iterator::value_type).name());
 	res.push_back(typeid(std::vector<int>::iterator::difference_type).name());
-	res.push_back(typeid(std::vector<int>::iterator::iterator_type).name());
 	res.push_back(typeid(std::vector<int>::iterator::pointer).name());
 	res.push_back(typeid(std::vector<int>::iterator::reference).name());
 	res.push_back(typeid(std::vector<int>::reverse_iterator::iterator_category).name());
@@ -22,7 +21,6 @@ bool iterator_traits_test() {
 	res2.push_back(typeid(_vector<int>::iterator::iterator_category).name());
 	res2.push_back(typeid(_vector<int>::iterator::value_type).name());
 	res2.push_back(typeid(_vector<int>::iterator::difference_type).name());
-	res2.push_back(typeid(_vector<int>::iterator::iterator_type).name());
 	res2.push_back(typeid(_vector<int>::iterator::pointer).name());
 	res2.push_back(typeid(_vector<int>::iterator::reference).name());
 	res2.push_back(typeid(_vector<int>::reverse_iterator::iterator_category).name());
--- ft_containers-unit-test/sources/vector_tests/insert(value).cpp
+++ ft_containers-unit-test/sources/vector_tests/insert(value).cpp
@@ -37,23 +37,7 @@ std::vector<int> insert_test_1(std::vector<T> vector) {
     g_start1 = timer();
     v.push_back(*(vector.insert(vector.end() - 8000000, 44)));
     g_end1 = timer();
-    v.push_back(vector.size());
-    v.push_back(vector.capacity());
-    std::unique_ptr<B> k2(new B(3));
-    std::unique_ptr<B> k3(new B(-4));
-//    std::unique_ptr<B> k4(new B(-1));
-    std::vector<A> vv;
-    std::vector<B*> v1;
 
-    v1.push_back(&(*k2));
-    v1.push_back(&(*k3));
-//    v1.push_back(&(*k4));
-
-    try { vv.insert(vv.begin(), v1.begin(), v1.end()); }
-    catch (...) {
-    	v.push_back(vv.size());
-    	v.push_back(vv.capacity());
-    }
     return v;
 }
 
@@ -64,23 +48,7 @@ std::vector<int> insert_test_1(_vector<T> vector) {
     g_start2 = timer();
     v.push_back(*(vector.insert(vector.end() - 8000000, 44)));
     g_end2 = timer();
-    v.push_back(vector.size());
-    v.push_back(vector.capacity());
-    std::unique_ptr<B> k2(new B(3));
-    std::unique_ptr<B> k3(new B(-4));
-//    std::unique_ptr<B> k4(new B(-1));
-    _vector<A> vv;
-    _vector<B*> v1;
 
-    v1.push_back(&(*k2));
-    v1.push_back(&(*k3));
-//    v1.push_back(&(*k4));
-
-    try { vv.insert(vv.begin(), v1.begin(), v1.end()); }
-    catch (...) {
-    	v.push_back(vv.size());
-    	v.push_back(vv.capacity());
-    }
     return v;
 }
 
