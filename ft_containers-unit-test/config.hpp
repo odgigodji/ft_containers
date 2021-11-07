108a91e245c2bac583fd205bbce0352489ce1a6e
odgigodji <santa51@mail.ru>
2021-11-07 22:00:58 +0300

    ft::make_pair

--- ft_containers-unit-test/config.hpp
+++ ft_containers-unit-test/config.hpp
@@ -23,7 +23,7 @@
 //#define	_set	 			ft::set
 #define	_is_integral 		ft::is_integral
 #define	_enable_if 			ft::enable_if
-//#define	_pair 				ft::pair
-//#define	_make_pair 			ft::make_pair
+#define	_pair 				ft::pair
+#define	_make_pair 			ft::make_pair
 #define	_lexicographical	ft::lexicographical_compare
 #define	_equal				ft::equal
