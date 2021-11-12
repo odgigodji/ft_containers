da36d24d6e399fd529e1a8fd752364620c77c67e
odgigodji <santa51@mail.ru>
2021-11-13 00:45:53 +0300

    working tests 1.1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-#include "map.hpp"
 #include "ft.hpp"
+#include "map.hpp"
 #include "vector.hpp"
 #include "stack.hpp"
 
 
-//int main() {
-//	std::cout << "/*******************************************************************************\n"
-//				 "*________________________ft_containers from namerei____________________________*\n"
-//				 "*******************************************************************************/\n" << std::endl;
-//
-//	std::cout << "For test please open 'ft_containers-unit-test' directory\nAnd ";
-//}
+int main() {
+	std::cout << "\n/*******************************************************************************\n"
+				 "*________________________ft_containers from namerei____________________________*\n"
+				 "*******************************************************************************/\n" << std::endl;
+
+	std::cout << "/*******************************************************************************\n"
+				 "*___________For test please open 'ft_containers-unit-test' directory___________*\n"
+				 "*____________________And print in console ./start.sh___________________________*\n"
+				 "*******************************************************************************/" << std::endl;
+
+	ft::stack<int> my_stack;
+	ft::vector<char> my_vector;
+	ft::map<int, std::string> my_map;
+
+	return 0;
+}
 
